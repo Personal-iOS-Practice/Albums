3c99cd8c128b8696125dd930aa2b1cb65b0afbca
Waseem Idelbi <idelbiw@gmail.com>
2022-06-21 22:29:14 -0400

    Added UI for the Detail table view controller, its cell, and files for both

---
+++ Albums/Albums/SongTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Waseem Idelbi on 6/21/22.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+//MARK: - IBOutlets
+    @IBOutlet weak var songTitleTextField: UITextField!
+    @IBOutlet weak var songDurationTextField: UITextField!
+    
+//MARK: - IBActions
+    // When the user taps the Add Song button
+    @IBAction func addSongButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
