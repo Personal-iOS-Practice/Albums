05a453b15a988dc402d2cb2a9fe13d453ea6a838
Waseem Idelbi <idelbiw@gmail.com>
2022-06-27 23:43:44 -0400

    saving changes

--- Albums/Albums/Album.swift
+++ Albums/Albums/Album.swift
@@ -7,34 +7,63 @@
 
 import Foundation
 
-//MARK: - Main Struct -
-struct Album {
+struct Album: Decodable {
+
+//MARK: - Properties -
     var name: String
     var artist: String
     var id: String
     var genres: [String]
-    var coverArt: [ImageStringURL]
+    var coverArt: [URL]
     var songs: [Song]
-}
-
-//MARK: - Helper Structs -
-
-// Main image url struct
-struct ImageStringURL {
-    var url: String
-}
-
-// Main Song struct
-struct Song {
-    var id: String
-    var name: Name
-    var duration: Duration
-}
-/// Song's helper struct
-struct Name {
-    var title: String
-}
-/// Song's helper struct
-struct Duration {
-    var duration: String
+    
+//MARK: - Coding keys enum
+    enum AlbumKeys: String, CodingKey {
+        
+        case name
+        case artist
+        case id
+        case genres
+        
+        case coverArt
+        enum CoverArtKeys: String, CodingKey {
+            case url
+        }
+        
+        case songs
+        
+    }
+    
+//MARK: - Decoder init method -
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: AlbumKeys.self)
+        var genresContainer = try container.nestedUnkeyedContainer(forKey: .genres)
+        var coverArtArrayContainer = try container.nestedUnkeyedContainer(forKey: .coverArt)
+        var songsArrayContainer = try container.nestedUnkeyedContainer(forKey: .songs)
+        
+        name = try container.decode(String.self, forKey: .name)
+        artist = try container.decode(String.self, forKey: .artist)
+        id = try container.decode(String.self, forKey: .id)
+        
+        genres = []
+        while !genresContainer.isAtEnd {
+            let genreString = try genresContainer.decode(String.self)
+            genres.append(genreString)
+        }
+        
+        coverArt = []
+        while !coverArtArrayContainer.isAtEnd {
+            let coverArtContainer = try coverArtArrayContainer.nestedContainer(keyedBy: AlbumKeys.CoverArtKeys.self)
+            let url = try coverArtContainer.decode(URL.self, forKey: .url)
+            coverArt.append(url)
+        }
+        
+        songs = []
+        while !songsArrayContainer.isAtEnd {
+            let songContainer = try songsArrayContainer.nestedContainer(keyedBy: Song.SongKeys.self)
+            let newSong = try Song(from: decoder)
+        }
+        
+    }
+    
 }
---
+++ Albums/Albums/Song.swift
@@ -0,0 +1,45 @@
+//
+//  Song.swift
+//  Albums
+//
+//  Created by Waseem Idelbi on 6/21/22.
+//
+
+import Foundation
+
+struct Song: Decodable {
+
+//MARK: - Properties -
+    var id: String
+    var duration: String
+    var name: String
+    
+//MARK: - Coding keys enum
+    enum SongKeys: String, CodingKey {
+        
+        case id
+        
+        case duration
+        enum DurationKey: String, CodingKey {
+            case duration
+        }
+        
+        case name
+        enum NameKey: String, CodingKey {
+            case title
+        }
+        
+    }
+    
+//MARK: - Decoder init method -
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: SongKeys.self)
+        let durationContainer = try container.nestedContainer(keyedBy: SongKeys.DurationKey.self, forKey: .duration)
+        let nameContainer = try container.nestedContainer(keyedBy: SongKeys.NameKey.self, forKey: .name)
+
+        id = try container.decode(String.self, forKey: .id)
+        duration = try durationContainer.decode(String.self, forKey: .duration)
+        name = try nameContainer.decode(String.self, forKey: .title)
+    }
+    
+}
