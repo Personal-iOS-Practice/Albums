2a09185c5e78bd28c5250af06c0b5d84390c36fe
Waseem Idelbi <idelbiw@gmail.com>
2022-06-29 18:22:15 -0400

    added the PUT method to send an Album to the firebase

--- Albums/Albums/AlbumController.swift
+++ Albums/Albums/AlbumController.swift
@@ -6,12 +6,72 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
 class AlbumController {
     
 //MARK: - Properties -
+    var albums: [Album] = []
+    let baseURL = URL(string: "https://albums-f1003-default-rtdb.firebaseio.com/")!
+    var database = Database.database().reference()
+    var encoder = JSONEncoder()
+    var decoder = JSONDecoder()
     
 //MARK: - Methods -
+    // Fetch albums - GET
+    func getAlbums(completion: @escaping (Error?) -> Void) {
+        
+        URLSession.shared.dataTask(with: baseURL) { data, response, error in
+            if let error = error {
+                completion(error)
+                return
+            }
+            if let response = response as? HTTPURLResponse {
+                if response.statusCode != 200 {
+                    print("ERROR: Fetching albums failed")
+                }
+            }
+            guard let data = data else { return }
+            
+            do {
+                let fetchedAlbums = try self.decoder.decode([String : Album].self, from: data)
+                for album in fetchedAlbums.values {
+                    self.albums.append(album)
+                }
+                return
+            } catch {
+                print("ERROR: Could not decode data, error message: \(error)")
+                return
+            }
+            
+        }
+        .resume()
+    }
+    // Upload album - PUT
+    func put(album: Album) {
+        
+        var request = URLRequest(url: baseURL.appendingPathComponent(album.id))
+        request.httpMethod = "PUT"
+        
+        URLSession.shared.dataTask(with: request) { _, response, error in
+            if let error = error {
+                print("ERROR: Failed to send album to API, error: \(error)")
+                return
+            }
+            if let response = response as? HTTPURLResponse {
+                if response.statusCode == 200 {
+                    print("Album was sent to API successfully!")
+                } else {
+                    print("ERROR: Failed to send album to API!")
+                    return
+                }
+            }
+        }
+        .resume()
+        
+    }
+    
+    // Test encoding and decoding methods
     func testDecodingExampleAlbum() {
         let urlPath = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json")!
         
@@ -24,7 +84,6 @@ class AlbumController {
         }
         
     }
-    
     func testEncodingExampleAlbum() {
         let urlPath = Bundle.main.url(forResource: "exampleAlbum", withExtension: "json")!
         
